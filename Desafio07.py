f832dbe168e1305a13ea16cc3b4a050f968bbf50
Hemerson Luan <140278282+Vultho-LK@users.noreply.github.com>
2024-08-04 21:24:19 -0300

    Add files via upload

---
+++ Desafio07.py
@@ -0,0 +1,4 @@
+# calcular a media de duas notas de um aluno
+n1 = float(input('digite sua primeira nota: '))
+n2 = float(input('digite sua segunda nota: '))
+print('a sua media é {}'.format((n1 + n2)/2))
---
+++ README.md
@@ -0,0 +1,3 @@
+Mostra a media entre dois numeros 
+
+<img src=https://github.com/user-attachments/assets/1c54f799-6bd2-4942-9cd9-f47da9301488 width=100>
